d991afe97f4767f5a5e0965bb2a4342b0e596fdc
Maicon Patrick Funke <maicon.funke@totvs.com.br>
2020-09-01 20:23:28 -0300

    fix(server): remove cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ const routes = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
 
 app.use(routes);
 
